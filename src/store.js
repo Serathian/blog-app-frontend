48eeb06d586914741901301bf0c2dabe97dc9e76
Jake <jake.a.reddy@gmail.com>
2021-02-26 10:56:55 +0200

    Installed redux, react-redux, dev-tools and thunk

---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const store = 0
+
+export default store
